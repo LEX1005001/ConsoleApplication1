cefdb58de27193be4108c4e3401c9b3c5c700e9a
LEX1005001 <AleX69.galkin@yandex.ru>
2023-01-01 18:52:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int sec = 0;
+	int min = 0;
+	int hour = 0;
+
+
+	cout << "-->";
+	cin >> sec;
+
+	if (sec >=60) {
+		min = sec / 60;
+		sec = sec % 60;
+	}
+	if (min>=60){
+		hour = min / 60;
+		min = min % 60;
+	}
+	cout << hour << "h " << min << "m " << sec << "sec";
+
+}	
