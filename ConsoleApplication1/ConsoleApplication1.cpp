23cc7bd89d7183a1281aee4062e7f20655f94406
LEX1005001 <117380414+LEX1005001@users.noreply.github.com>
2023-01-01 19:23:18 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,23 +1,16 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-	int sec = 0;
-	int min = 0;
-	int hour = 0;
+	int days = 0;
+	int weeks = 0;
 
+	cout << ":";
+	cin >> days;
 
-	cout << "-->";
-	cin >> sec;
-
-	if (sec >=60) {
-		min = sec / 60;
-		sec = sec % 60;
+	if (days >= 7) {
+		weeks = days / 7;
+		days = days % 7;
 	}
-	if (min>=60){
-		hour = min / 60;
-		min = min % 60;
-	}
-	cout << hour << "h " << min << "m " << sec << "sec";
-
-}	
+cout << weeks << " weeks" << " and " << days << " days";
+}
