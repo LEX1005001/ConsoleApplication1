b60e71ee9466bee2e01141d966c5422112bb42f9
LEX1005001 <117380414+LEX1005001@users.noreply.github.com>
2023-03-23 15:50:22 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,13 +1,45 @@
 #include <iostream>
 using namespace std;
+void fill_array(int* arr,int size) 
+{
+	for (int i = 0; i < size; i++) {
+		arr[i] = rand() % 100;
+	}
+}
+void print_array(int* arr, int size) {
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
+}
 int main()
 {
-	int dollar = 0;
-	int cent = 0;
-	double number = 0;
-	cout << "number : ";
-	cin >> number;
-	dollar = number;
-	cent = (number - dollar) * 100;
-	cout << dollar << "$ " << cent << "&";
+	srand(time(NULL));
+	setlocale(0, "ru");
+	int size1 = 5;
+	int* arr1 = new int[size1];
+
+	int size2 = 5;
+	int* arr2 = new int[size2];
+
+	int size3 = 5;
+	int* arr3 = new int[size3];
+
+	fill_array(arr1, size1);
+	fill_array(arr2, size2);
+
+	cout << "Первый массив: ";
+	print_array(arr1, size1);
+	cout << endl;
+
+	cout << "Второй массив: ";
+	print_array(arr2, size2);
+	cout << endl;
+
+	for (int i = 0; i < size1; i++) {
+		arr3[i] = arr1[i]+arr2[i];
+
+	}
+	
+	cout << "Финальный массив: ";
+	print_array(arr3, size3);
 }
